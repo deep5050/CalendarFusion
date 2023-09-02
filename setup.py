ba647d19e0e9e3382cab1c6f1ff485cfc98834d1
Dipankar Pal <dipankarpal5050@gmail.com>
2023-09-02 17:27:16 +0530

    Fix: updated setup.py to support pypi standard

--- setup.py
+++ setup.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(name='CalendarFusion',
-      version='1.0',
-      description='A python module to generate fancy markdown table based calendar',
-      author='Dipankar Pal',
-      author_email='dipankarpal5050@gmail.com',
-      url='http://github.com/deep5050/CalendarFusion',
-      download_url='https://github.com/deep5050/CalendarFusion/tarball/master',
-      packages=find_packages(),
-      license="MIT"
-     )
+setup(
+    name="CalendarFusion",
+    version="1.0",
+    description="A python module to generate fancy markdown table based calendar",
+    author="Dipankar Pal",
+    keywords="calendar markdown-table markdown calendar-to-table cli-app",
+    author_email="dipankarpal5050@gmail.com",
+    url="http://github.com/deep5050/CalendarFusion",
+    download_url="https://github.com/deep5050/CalendarFusion/tarball/master",
+    packages=find_packages(),
+    license="MIT",
+    classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+    'Programming Language :: Python :: 3 :: Only',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Programming Language :: Python :: Implementation :: PyPy',
+    'Topic :: Communications :: Email :: Filters',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
+    'Topic :: Internet :: WWW/HTTP :: Site Management',
+    'Topic :: Software Development :: Documentation',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Text Processing :: Filters',
+    'Topic :: Text Processing :: Markup :: HTML',
+    'Topic :: Text Processing :: Markup :: Markdown'
+]
+
+)
